a6b493d485829c179bba1c37d92055913b62272d
Nate <nate@qstreet.org>
2025-04-25 21:28:51 -0700

    First Commit

---
+++ embed.ts
@@ -0,0 +1,27 @@
+import { load } from "npm:cheerio"
+import ollama from "npm:ollama"
+
+const urls = [
+  "https://deno.com/blog/not-using-npm-specifiers-doing-it-wrong",
+  "https://deno.com/blog/build-database-app-drizzle",
+  "https://deno.com/blog/v2.1",
+]
+
+const embeddings = await Promise.all(
+  urls.map(async (url) => {
+    const res = await fetch(url)
+    const html = await res.text()
+
+    const $ = load(html)
+    const content = $("body").text()
+
+    const { embeddings } = await ollama.embed({
+      model: "mxbai-embed-large",
+      input: content,
+    })
+
+    return { embedding: embeddings[0], source: url }
+  })
+)
+
+await Deno.writeTextFile("embeddings.json", JSON.stringify(embeddings))
---
+++ test.ts
@@ -0,0 +1,10 @@
+const state = {
+  step: "start",
+  messages: [{ role: "user", content: "What is your name?" }],
+}
+
+while (state.step !== "end") {
+  if (state.step === "start") {
+    //
+  }
+}
